1eed422b75ededb63cb4e255702f6f516bc411a7
YamaP <yamap0506e@gmail.com>
2018-03-19 20:49:03 +0900

    scene create
    make move scene

---
+++ EntrySheet/EntrySheet/SceneData.swift
@@ -0,0 +1,21 @@
+//
+//  SceneData.swift
+//  EntrySheet
+//
+//  Created by Tanko on 2018/03/19.
+//  Copyright © 2018年 Tanko. All rights reserved.
+//
+
+import Foundation
+class SceneData {
+    func admin() -> String{
+        return "Administrator"
+    }
+    func general() -> String{
+        return "GeneralUser"
+    }
+    
+    
+}
+
+
--- EntrySheet/EntrySheet/ViewController.swift
+++ EntrySheet/EntrySheet/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sceneData = SceneData()
+    
+    //戻るボタンを押したときの動作
+    @IBAction func goBack(_ segue:UIStoryboardSegue) {}
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+//    @IBAction func AdminSignIn(_ sender: Any) {
+//        let userState = sceneData.admin()
+//        let storyboard: UIStoryboard = UIStoryboard(name: userState, bundle: nil)
+//        let nextView = storyboard.instantiateInitialViewController()
+//        present(nextView!, animated: true, completion: nil)
+//    }
 }
 
